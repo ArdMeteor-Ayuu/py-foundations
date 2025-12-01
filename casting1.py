7290258ec6828253e783d0c3b622cfd30f209d33
Ayush Rathour <ayush.si.rathour@gmail.com>
2025-12-01 18:24:34 +0530

    Add files via upload
    Day1 casting

---
+++ casting1.py
@@ -0,0 +1,14 @@
+x = '10'
+y = int(x)
+z = float(y)
+b = bool(x)
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(b))
+
+
+
+# x = int("10.5")  # error cannot convert string with decimal point to int
+# y = float("ten")  # error cannot convert non-numeric string to float
+# b = int("True")   # error cannot convert non-numeric string to int
